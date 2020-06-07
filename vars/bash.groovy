b3d7bc049c38a52cd536849b2bc2358d5760748b
Levi Blaney <shadycuz@gmail.com>
2020-06-07 12:30:39 -0400

    change exception from local to library

--- vars/bash.groovy
+++ vars/bash.groovy
@@ -1,3 +1,5 @@
+import org.dsty.exceptions.ScriptError
+
 /**
  * Formats a bash script by adding the shebang,
  * setting the verbose level and sourcing bashrc.
@@ -146,44 +148,4 @@ private List<String> getOutputs(){
   List results = [stdOut, stdErr, output].collect { it.replaceAll('(?m)^\\+.*(?:\r?\n)?', '') }
 
   return results
-}
-
-
-/**
- * Custom Exception thrown by the bash global variable.
- */
-class ScriptError extends Exception {
-
-  /**
-   * The contents of stdOut from the bash script.
-   */
-  String stdOut
-
-  /**
-   * The contents of stdErr from the bash script.
-   */
-  String stdErr
-
-  /**
-   * The combined contents of stdOut and stdErr from the bash script.
-   */
-  String output
-
-  /**
-   * The exitCode from the bash script.
-   */
-  Integer exitCode
-
-  ScriptError(String stdOut, String stdErr, String output, Integer exitCode) {
-    super("Script exitCode was ${exitCode} and stdout:\n${stdErr}")
-    this.stdOut = stdOut
-    this.stdErr = stdErr
-    this.output = output
-    this.exitCode = exitCode
-    // this.errorMessage = "Script exited ${this.exitCode}. stderr was:\n${this.stdErr}"
-  }
-
-  String getFullMessage() {
-      return "Script exited ${this.exitCode}. Output was:\n${this.output}"
-  }
 }
