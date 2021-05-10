5099c2605dc62ed32bf43989c4a0361396b45bad
Levi Blaney <shadycuz@gmail.com>
2021-05-10 07:30:26 -0400

    init commit of Github actions workflow

---
+++ src/org/dsty/github/actions/Workflow.groovy
@@ -0,0 +1,117 @@
+package org.dsty.github.actions
+
+import org.dsty.bash.BashClient
+import org.dsty.bash.Result
+import org.dsty.bash.ScriptError
+import org.dsty.logging.LogClient
+
+/**
+ * Runs Github Actions workflows using the <a href="https://github.com/nektos/act"><strong>act</strong></a> CLI tool.
+ * <p>To run a Github workflow in a jenkins ScriptedPipeline:
+ * <pre>{@code
+ * import org.dsty.github.actions.Workflow
+ *node() {
+ *  Workflow workflow = new Workflow(this)
+ *  workflow()
+ *&#125;}</pre>
+ * <p> For more details see {@link #call(java.lang.String) call}.
+ */
+class Workflow implements Serializable {
+
+  /**
+   * Workflow script representing the jenkins build.
+   */
+  private final Object steps
+
+  /**
+   * Logging client
+   */
+  private final LogClient log
+
+  /**
+   * Bash Client
+   */
+  private final BashClient bash
+
+  /**
+   * The version of <strong>act</strong> to install.
+   */
+  String version
+
+  /**
+   * Default Constructor
+   * @param steps The workflow script representing the jenkins build.
+   */
+  Workflow(Object steps) {
+    this.steps = steps
+    this.log = new LogClient(steps)
+    this.bash = new BashClient(steps)
+  }
+
+  /**
+    * Installs and then runs <strong>act</strong>.
+    * <p> The default event is <strong>push</strong> to change it to <strong>pull_request</strong>:</p>
+    * <pre>{@code
+    * import org.dsty.github.actions.Workflow
+    *node() {
+    *  Workflow workflow = new Workflow(this)
+    *  workflow('pull_request')
+    *&#125;}</pre>
+    * @param args The arguments passed directly to <strong>act</strong>. See <strong>act</strong> <a href="https://github.com/nektos/act#commands">docs</a>.
+    * @return The output from the <strong>act</strong> command.
+    */
+  String call(String args = '') throws ScriptError {
+    this.install()
+
+    return this.run(args)
+  }
+
+  /**
+    * Installs <strong>act</strong> by first checking if it is already installed and
+    * if not then installs the latest version. The property {@link #version} can be set to install
+    * a specific version.
+    */
+  void install() throws ScriptError {
+
+    this.log.info('Installing Act.')
+
+    Result result = this.bash.ignoreErrors(/act --version | cut -d' ' -f3/, false, true)
+
+    if (result.exitCode == 0 && !this.version) {
+
+      this.log.info("Version ${result.stdOut} of act is already installed.")
+      return
+
+    }
+
+    if (!this.version) {
+
+      this.log.info('Downloading the latest version.')
+      this.bash.call('curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash')
+
+    }
+
+    this.log.info("Downloading version ${this.version}.")
+
+    this.steps.dir(this.steps.env.WORKSPACE) {
+
+      this.bash.call('curl https://raw.githubusercontent.com/nektos/act/master/install.sh -o .install_act | sudo bash .install_act')
+
+    }
+
+  }
+
+  /**
+    * Calls the <strong>act</strong> binary with the supplied arugments.
+    * @param args The arguments passed directly to <strong>act</strong>. See <strong>act</strong> <a href="https://github.com/nektos/act#commands">docs</a>.
+    * @return The output from the <strong>act</strong> command.
+    */
+  String run(String args) throws ScriptError {
+
+    Result result = this.bash.call("act ${args}")
+
+    return result.output
+
+  }
+
+}
