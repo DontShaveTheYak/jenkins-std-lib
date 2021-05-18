d25ca7ac7cfca984d5cb7ac135916d278745442f
Levi Blaney <shadycuz@gmail.com>
2021-05-18 03:16:27 -0400

    update/add tests for Step

--- jobs/github/actions/step_example.groovy
+++ jobs/github/actions/step_example.groovy
@@ -1,14 +1,23 @@
 /* groovylint-disable DuplicateMapLiteral, DuplicateStringLiteral, UnnecessaryParenthesesForMethodCallWithClosure, UnusedVariable */
 @Library('pipeline-library')
 
-import org.dsty.github.Action
+import org.dsty.github.actions.Step
 
 node() {
+
     String cps = sh(script: '#!/bin/bash\nset +x; > /dev/null 2>&1\necho Test for CPS issue', returnStdout: true)
 
-    Action action = new Action(this)
+    Step action = new Step(this)
 
-    Map outputs = action.uses('actions/hello-world-docker-action@master').with(['who-to-greet': 'Jenkins-Std-Lib'])
+    Map options = [
+        'name': 'Get build time.',
+        'uses': 'actions/hello-world-docker-action@master',
+        'with': [
+            'who-to-greet': 'Mona the Octocat'
+        ]
+    ]
+
+    Map outputs = action(options)
 
     if (!outputs.time) {
         error('Should have an output named time.')
---
+++ jobs/github/actions/tests/test_step.groovy
@@ -0,0 +1,39 @@
+/* groovylint-disable DuplicateMapLiteral, DuplicateStringLiteral, Println, UnnecessaryParenthesesForMethodCallWithClosure, UnusedVariable */
+@Library('pipeline-library')
+
+import org.dsty.github.actions.Step
+
+node() {
+
+    String cps = sh(script: '#!/bin/bash\nset +x; > /dev/null 2>&1\necho Test for CPS issue', returnStdout: true)
+
+    Step action = new Step(this)
+
+    Map options = [
+      'name': 'TestIllegalArgument'
+    ]
+
+    try {
+      Map outputs = action(options)
+      error("Should not run if 'uses' or 'run' not provided.")
+    } catch (IllegalArgumentException ex) {
+      println('Threw the correct exception.')
+    }
+
+    options = [
+      'name': 'TestConditional',
+      'uses': 'actions/hello-world-docker-action@master',
+      'with': [
+        'who-to-greet': 'Mona the Octocat'
+      ],
+      'if': false
+    ]
+
+    Map outputs = action(options)
+
+    if (outputs) {
+      error('Should haved skipped running the step.')
+    }
+
+    cps = sh(script: '#!/bin/bash\nset +x; > /dev/null 2>&1\necho Test for CPS issue', returnStdout: true)
+}
