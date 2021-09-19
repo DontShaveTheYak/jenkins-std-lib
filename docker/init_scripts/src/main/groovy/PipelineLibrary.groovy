efc81e6988090156d63ee8b9ba2971064778d037
Levi Blaney <shadycuz@gmail.com>
2021-09-19 02:50:07 -0400

    use FSCM to configure library

--- docker/init_scripts/src/main/groovy/PipelineLibrary.groovy
+++ docker/init_scripts/src/main/groovy/PipelineLibrary.groovy
@@ -3,6 +3,8 @@ import jenkins.plugins.git.GitSCMSource
 import org.jenkinsci.plugins.workflow.libs.GlobalLibraries
 import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
 import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+import org.jenkinsci.plugins.workflow.libs.SCMRetriever
+import hudson.plugins.filesystem_scm.FSSCM
 
 println('== Configuring the Jenkins Pipeline library')
 
@@ -14,50 +16,13 @@ final File file = new File('/var/jenkins_home/pipeline-library/src')
 if (file.exists()) {
     println("===== Adding local ${libName}")
 
-    StringBuilder stdout = new StringBuilder()
-    StringBuilder stderr = new StringBuilder()
+    FSSCM scm = new FSSCM(file.getParent(), false, false, null)
 
-    File script = File.createTempFile('temp', '.sh')
+    lc = new LibraryConfiguration(libName, new SCMRetriever(scm))
 
-    String lib = '/var/jenkins_home/lib'
-
-    script.write(
-        """#!/usr/bin/env bash
-        mkdir ${lib} || true
-        cp -r /var/jenkins_home/pipeline-library/* ${lib}
-
-        git config --global user.email ""admin@non.existent.email""
-        git config --global user.name "Jenkinsfile-runner"
-
-        cd ${lib}
-        rm -rf .git || true
-        git init
-        git add --all
-        git commit -m init
-        """
-    )
-
-    Process proc = "bash ${script.absolutePath}".execute()
-
-    proc.consumeProcessOutput(stdout, stderr)
-    proc.waitForOrKill(5000)
-
-    if (stderr) {
-        println("stdout: ${stdout}")
-        println("stderr: ${stderr}")
-    }
-
-    GitSCMSource scm = new GitSCMSource(libName, lib, null, null, null, false)
-
-    lc = new LibraryConfiguration(libName, new SCMSourceRetriever(scm))
-    lc.with {
-        implicit = true
-        defaultVersion = repoBranch
-    }
 } else {
     println("===== Adding remote ${libName}")
 
-    // TODO: change defaults once https://github.com/jenkins-infra/pipeline-library/pull/78 is merged
     String repoURL = 'https://github.com/DontShaveTheYak/jenkins-std-lib'
 
     println("===== Using the Pipeline library from ${repoURL}")
