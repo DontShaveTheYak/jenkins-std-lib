f22e4d2498b23ca19a4e4f5ea5bd5b58881c4dcf
Levi Blaney <shadycuz@gmail.com>
2021-09-19 09:29:35 -0400

    enhance groovy foc for jenkins package.

--- src/org/dsty/jenkins/package-info.groovy
+++ src/org/dsty/jenkins/package-info.groovy
@@ -1,4 +1,8 @@
 /**
- * Get information about the Jenkins Instance.
+ * Get information about the Jenkins controller or it's agents.
+ * <p>
+ * For information about the Jenkins controller, like what plugins are installed,
+ * see <a href="Instance.html">Instance</a>. To get access to the current build's
+ * steps or environment variables see <a href="Build.html">Build</a>.
  */
 package org.dsty.jenkins
