f65f516fbc1c558bafd2181fb020759b5a633a89
Levi Blaney <shadycuz@gmail.com>
2021-09-30 05:47:50 -0400

    update package-info with links to other subpackages

--- src/org/dsty/system/package-info.groovy
+++ src/org/dsty/system/package-info.groovy
@@ -1,6 +1,8 @@
 /**
- * This package provides operating system independent functionality.
+ * The packages under the <code>system</code> namespace provide information about the hardware
+ * and software the Jenkins build is being executed on.
  * <p>
- * If you want to work with files/directories see <a href="Path.html">Path</a>.
+ * To get information about the current <code>system</code> see <a href="Platform.html">Platform</a>.<br>
+ * To interact with programs and files see <a href="../system/os/package-summary.html">org.dsty.system.os</a>.
  */
 package org.dsty.system
