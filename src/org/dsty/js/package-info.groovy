a61b9617d95c60b9fad4f121cb02a38ab1042f72
Levi Blaney <shadycuz@gmail.com>
2022-07-14 14:25:17 -0600

    wip laptop backup

---
+++ src/org/dsty/js/package-info.groovy
@@ -0,0 +1,7 @@
+/**
+ * This package provides tools to work with Javascript and it's releated echo systems like
+ * Node and Typescript.
+ * <p>
+ * If you want to work with files/directories see <a href="Path.html">Path</a>.
+ */
+package org.dsty.js
