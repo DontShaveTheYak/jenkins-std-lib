a57d0141ffbaff1a39fe9bc5e7151926cffe6dce
Levi Blaney <shadycuz@gmail.com>
2022-07-14 14:25:17 -0600

    wip laptop backup

---
+++ src/org/dsty/js/node/package-info.groovy
@@ -0,0 +1,6 @@
+/**
+ * This package provides tools to work with Node and NPM.
+ * <p>
+ * If you want to work with files/directories see <a href="Path.html">Path</a>.
+ */
+package org.dsty.js.node
