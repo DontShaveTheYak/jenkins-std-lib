1409ace68fe370461944d0cfdeca755c08c25e2f
Levi Blaney <shadycuz@gmail.com>
2022-11-06 17:38:42 +0000

    dont pass in the docker socket anymore

--- src/org/dsty/github/actions/JavaScriptAction.groovy
+++ src/org/dsty/github/actions/JavaScriptAction.groovy
@@ -27,15 +27,10 @@ class JavaScriptAction extends DockerAction implements GithubAction {
         String actionDir = '/github/action'
         String workspace = "${this.options.workspace}/${this.name}"
 
-        if (this.steps.env.DIND_JENKINS_HOME) {
-            List parts = this.options.workspace.split('/workspace/')
-
-            workspace = "${this.steps.env.DIND_JENKINS_HOME}/workspace/${parts[1]}/${this.name}"
-        }
-
         this.metadata.runs.entrypoint = 'node'
-        this.metadata.runs.args = ["${actionDir}/${this.metadata.runs.main}"
-    ]
+        this.metadata.runs.args = [
+            "${actionDir}/${this.metadata.runs.main}"
+        ]
         this.metadata.runs['pre-entrypoint'] = this.metadata.runs.pre ?: ''
         this.metadata.runs['post-entrypoint'] = this.metadata.runs.post ?: ''
 
@@ -44,43 +39,13 @@ class JavaScriptAction extends DockerAction implements GithubAction {
         this.log.info(this.log.pprint(this.options))
         this.log.info(this.log.pprint(this.metadata))
 
-        Map outputs = this.actionRun('node:12.20.1-buster-slim')
+        String nodeVersion = this.metadata.runs.using
+
+        Number version = nodeVersion.find( /\d+/ ).toInteger()
+
+        Map outputs = this.actionRun("node:${version}")
 
         return outputs
-    }
-
-    /**
-     * Parses the input for Github Actions outputs.
-     * @param input to search for Github Actions outputs.
-     * @returns the outputs found.
-     */
-    @NonCPS
-    @Override
-    Map parseOutputs(String input) {
-        Map outputs = [:]
-
-        List matches = (input =~ /(?m)##\[set-output.*$/).findAll()
-
-        for (match in matches) {
-            String outputName = (match =~ /(?m)(?<=name=).*(?=;])/).findAll().first()
-            String outputValue = (match =~ /(?m);](.*$)/).findAll().first()[1]
-
-            outputs[outputName] = outputValue
-        }
-
-        return outputs
-    }
-
-    /**
-     * Removes the Github Actions outputs so it can be displayed
-     * to the user.
-     * @param input to search for Github Actions outputs.
-     * @returns the input free of any Github Action outputs.
-     */
-    @NonCPS
-    @Override
-    String cleanOutput(String input) {
-        return (input =~ /(?m)##\[set-output.*$/).replaceAll('')
     }
 
 }
--- tests/test_scm/test_Generic.py
+++ tests/test_scm/test_Generic.py
@@ -2,14 +2,21 @@ import pytest
 
 from tests.conftest import Container
 
+
 @pytest.fixture()
 def job_folder():
-    return 'scm'
+    return "scm"
+
 
 def test_generic_unit(container: Container, job_folder):
 
     container(f"{job_folder}/tests/test_generic.groovy")
 
+
+@pytest.mark.xfail(
+    strict=True,
+    reason="Broken because of https://issues.jenkins.io/browse/JENKINS-70025",
+)
 def test_generic_example(container: Container, job_folder):
 
     job_output = container(f"{job_folder}/generic_example.groovy")
